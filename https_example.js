c49cf3f20bfbf4c04f2ac0cebd322cdabd3a4001
Ilinca Pascu <xipascu@gmail.com>
2017-10-09 23:38:55 +0000

    second https example push.

--- https_example.js
+++ https_example.js
@@ -1,3 +1,19 @@
 var https = require('https');
 
-console.log('I did it!');
+var options = {
+  host: 'www.example.org',
+  path: '/'
+};
+
+//this is called by https when request is made
+
+var callback = function() {
+  console.log('In reposnse handler callback');
+}
+
+
+console.log("I'm about to make the request.");
+
+https.request(options, callback).end();
+
+console.log("I've made the request!");
